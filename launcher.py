e5bb9611a6fb251cb03006631e55e897f5927552
Daloshka <36123350+Daloshka@users.noreply.github.com>
2022-01-07 23:15:47 +0800

    Add files via upload

--- launcher.py
+++ launcher.py
@@ -8,7 +8,7 @@ ua = UserAgent()
 def collect_data():
     # get html page
     response = requests.get(
-        url='https://api.dmarket.com/exchange/v1/market/items?side=market&orderBy=best_deals&orderDir=desc&title=&priceFrom=300&priceTo=1000&treeFilters=category_1%5B%5D=not_souvenir,categoryPath%5B%5D=pistol,categoryPath%5B%5D=smg,categoryPath%5B%5D=rifle,categoryPath%5B%5D=sniper%20rifle,categoryPath%5B%5D=shotgun,categoryPath%5B%5D=machinegun&gameId=a8db&types=dmarket&cursor=&limit=100&currency=USD&platform=browser&isLoggedIn=true',
+        url='https://api.dmarket.com/exchange/v1/market/items?side=market&orderBy=best_deals&orderDir=desc&title=&priceFrom=300&priceTo=700&treeFilters=category_1%5B%5D=not_souvenir,categoryPath%5B%5D=pistol,categoryPath%5B%5D=smg,categoryPath%5B%5D=rifle,categoryPath%5B%5D=sniper%20rifle,categoryPath%5B%5D=shotgun,categoryPath%5B%5D=machinegun&gameId=a8db&types=dmarket&cursor=&limit=100&currency=USD&platform=browser&isLoggedIn=true',
         headers={'user-agent': f'{ua.random}'}
     )
 
@@ -28,10 +28,18 @@ def collect_data():
             sTitles = ''
             for j in i.get('extra').get('stickers'):
                 sTitles += j['name'] + '\n'
-            print(f"{i.get('price').get('USD')} руб   {i.get('title')} \n{sTitles}")
+            with open('WeaponTitles.txt', 'a', encoding="utf-8") as file:
+                file.write(f"{i.get('price').get('USD')} руб   {i.get('title')}\nfloat={i.get('extra').get('floatValue')} \n{sTitles}\n")
+            print(f"{i.get('price').get('USD')} руб   {i.get('title')}\nfloat={i.get('extra').get('floatValue')} \n{sTitles}")
 
 def main():
+    clearWeaponTitles()
     collect_data()
+
+# function to clear saved info in Weapon Titles
+def clearWeaponTitles():
+    with open('WeaponTitles.txt', 'w') as file:
+        file.write('')
 
 
 if __name__ == "__main__":
