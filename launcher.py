2cf9022fb7eae9100cb66049f915b43349bf1fa2
Daloshka <36123350+Daloshka@users.noreply.github.com>
2022-02-10 13:37:06 +0800

    Add files via upload

--- launcher.py
+++ launcher.py
@@ -4,11 +4,15 @@ import json
 import csv
 import win10toast
 from tkinter import *
+import telebot
+
 
 # create UserAgent
 ua = UserAgent()
 
-
+# Telebot
+# bot = telebot.TeleBot('5234922052:AAGAPSSPwhrSj3oe2wS3sS151HHadX6NWZ0')
+# chat_id = -1001625897184
 
 def skin_prices(priceFrom=100,priceTo=3000):
     priceFromTo = []
@@ -21,7 +25,7 @@ def skin_prices(priceFrom=100,priceTo=3000):
     print(priceFromTo)
     return priceFromTo
 
-# notificatoion
+# windows notificatoion
 def notify(skin = "Example", skin_price = "123", sTotal = "123"):
     toast = win10toast.ToastNotifier()
     notification = toast.show_toast(title=f"{skin}", msg=f"Цена скина = {skin_price}  руб.\nЦена наклеек = {sTotal} руб.", duration=4)
@@ -60,12 +64,12 @@ def collect_data(priceFrom, priceTo):
                         if j['name'] in line:
                             sTotal = float(sTotal) + float(line[1])      
                 sTitles += j['name'] + '\n'
-            if sTotal >= min_total_sticker and i.get('discount') >= 15:
+            if sTotal >= min_total_sticker.get() and i.get('discount') >= 15:
                 with open('WeaponTitles.txt', 'a', encoding="utf-8") as file:
                     file.write(f"{skin_price} руб   {i.get('title')}\nfloat={i.get('extra').get('floatValue')} \n{sTitles}\nDiscount -{i.get('discount')}%\nStickers price: {sTotal}\n\n-----------------------------------------------------------------------------------------------------\n\n")
                 print(f"{skin_price} руб   {i.get('title')}\nfloat={i.get('extra').get('floatValue')} \n{sTitles}\nDiscount -{i.get('discount')}%\nStickers price: {sTotal}\n-----------------------------------------------------------------------------------------------------\n\n")
                 #notify(skin, skin_price, sTotal)
-
+                msgs[skin] = discount, skin_price, skin, sTotal
 # function to clear saved info in Weapon Titles
 def clearWeaponTitles():
     with open('WeaponTitles.txt', 'w') as file:
@@ -76,12 +80,21 @@ def main():
     for i,j in getPp():
         collect_data(i,j)
 
+# 
 def getPp():
         priceFromTo = skin_prices(int(price_from_enter.get()),int(price_to_enter.get()))
         return priceFromTo
 
+# get info about skin for Telegram bot
+def saveInfo(skin_price, skin, discount, sTotal):
+    msgs.append(f"{skin_price} руб,\n{skin}\n-{discount}%\n{sTotal}")
+
+#telegram msgs
+msgs = {}
 if __name__ == "__main__":
-    min_total_sticker = 400
+    
+    min_total_sticker = 100
+
     #creat Menu
     root = Tk()
     root.title("Dmarket Helper")
@@ -95,27 +108,38 @@ if __name__ == "__main__":
     bg_logo.place(x=0,y=0, relheight=1, relwidth=1)
 
     # Enter boxes
-    price_from_enter = StringVar()
-    price_to_enter = StringVar()
-    message_entry = Entry(textvariable=price_from_enter)
-    message_entry.place(relx=.45, rely=.85, anchor="c")
-    message_entry2 = Entry(textvariable=price_to_enter)
-    message_entry2.place(relx=.55, rely=.85, anchor="c")
-
-    butt = Button(root, text="Search", bg="#C9ACAE")
-    butt.place(relx=0.48, rely=0.91)
-    butt.config(command=main)
-    butt2 = Button(root, text="Set Price", bg="#C9ACAE")
-    butt2.place(relx=0.48, rely=0.87)
-    butt2.config(command=getPp)
-
     # Input price from - to
-    # Price from  - to in RUB
-    
-    
+    price_from_enter = IntVar()
+    price_from_enter.set(100)
+    price_to_enter = IntVar()
+    price_to_enter.set(600)
+    min_discount = IntVar()
+    min_discount.set(10)
+    min_total_sticker = IntVar()
+    min_total_sticker.set(300)
 
 
+    lbl = Label(root, text="from", font=("Hemi Head", 15), bg="#05151f", fg="#FFFFFF")
+    lbl2 = Label(root, text="to", font=("Hemi Head", 15), bg="#05151f", fg="#FFFFFF")
+    lbl3 = Label(root, text="discount", font=("Hemi Head", 15), bg="#05151f", fg="#FFFFFF")
+    lbl4 = Label(root, text="stickers", font=("Hemi Head", 15), bg="#05151f", fg="#FFFFFF")
+    
+    message_entry = Entry(textvariable=price_from_enter, width=10, justify=LEFT)
+    message_entry.grid(column=1, row=0, sticky=W)
+    message_entry2 = Entry(textvariable=price_to_enter, width=10, justify=LEFT)
+    message_entry2.grid(column=1, row=1, sticky=W)
+    message_entry3 = Entry(textvariable=min_discount, width=10, justify=LEFT)
+    message_entry3.grid(column=1, row=2, sticky=W)
+    message_entry4 = Entry(textvariable=min_total_sticker, width=10, justify=LEFT)
+    message_entry4.grid(column=1, row=3, sticky=W)
+    lbl.grid(column=0, row=0)
+    lbl2.grid(column=0, row=1)
+    lbl3.grid(column=0, row=2)
+    lbl4.grid(column=0, row=3)
+    
+    butt = Button(root, text="Search", bg="#05151f", fg="#FFFFFF")
+    butt.grid(column=0, row=4, columnspan=2)
+    butt.config(command=main)
+
+    #bot.polling()
     root.mainloop()
-
-
-    #main()
